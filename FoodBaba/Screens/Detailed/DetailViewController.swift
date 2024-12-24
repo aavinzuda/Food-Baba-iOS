596500ce2e813f2ef5f99b454744eb672505ae8f
Aakash VInzuda <aakashvinzuda222@gmail.com>
2024-12-25 00:44:06 +0530

    Modified the DetailedViewController.

--- FoodBaba/Screens/Detailed/DetailViewController.swift
+++ FoodBaba/Screens/Detailed/DetailViewController.swift
@@ -9,7 +9,24 @@ import UIKit
 
 
 final class DetailViewController: UIViewController {
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var subTitleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func setup(for model: PopularBrandModel) {
+        DispatchQueue.main.async {
+            self.imageView.image = model.image
+            self.titleLabel.text = model.name
+            self.subTitleLabel.text = model.getKeyWordsString()
+        }
+    }
+    
+    @IBAction func didTapBackButton(_ sender: UIButton) {
+        self.dismiss(animated: true)
     }
 }
