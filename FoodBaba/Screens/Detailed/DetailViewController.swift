28a66ecf430ce24309fff2bfa28f9efb9b200df5
Aakash VInzuda <aakashvinzuda222@gmail.com>
2024-12-24 23:22:20 +0530

    Added DetailViewController

---
+++ FoodBaba/Screens/Detailed/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  FoodBaba
+//
+//  Created by A. A. Vinzuda on 24/12/24.
+//
+
+import UIKit
+
+
+final class DetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
